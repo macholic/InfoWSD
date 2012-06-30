8afdce3f228e0c48f6c04676ce3ec7cc3a646464
macholic <wsd.macholic@gmail.com>
2012-06-30 16:02:44 +0900

    add api

--- lib/InfoWSD/Web/Dispatcher.pm
+++ lib/InfoWSD/Web/Dispatcher.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::Lite;
+use JSON::Syck;
 
 any '/' => sub {
     my ($c) = @_;
@@ -13,6 +14,22 @@ post '/account/logout' => sub {
     my ($c) = @_;
     $c->session->expire();
     $c->redirect('/');
+};
+
+any '/api/test' => sub {
+    my ($c) = @_;
+    my $t_data = [
+        {
+            ROOM => "A",
+            USING => 0,
+        },
+        {
+            ROOM => "B",
+            USING => 1,
+        },
+    ];
+    my $json = JSON::Syck::Dump($t_data);
+    $c->render('api/test.json', { json => $json });
 };
 
 1;
