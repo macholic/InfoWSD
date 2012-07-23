1a84d8ec76360dc2692ccd9c9f7d1bb0033b7b98
macholic <wsd.macholic@gmail.com>
2012-07-23 19:37:06 +0900

    fix

--- lib/InfoWSD/Web/Dispatcher.pm
+++ lib/InfoWSD/Web/Dispatcher.pm
@@ -21,8 +21,8 @@ any '/' => sub {
 any '/api/pc/' => sub {
     my ($c) = @_;
     my $pc_room_nows = $c->db->search('pc_room_now', {}, { order_by => 'id asc' });
+    my %data;
     if ($c->req->param('fmt') eq 'json') {
-        my %data;
         while (my $pc_room_now = $pc_room_nows->next) {
             $data{$pc_room_now->name} = +{
                 status => $pc_room_now->status,
@@ -42,8 +42,15 @@ any '/api/pc/' => sub {
 
 any '/api/class/' => sub {
     my ($c) = @_;
-    my $json;
-    $c->render('api/class/default.json', { json => $json });
+    my $data;
+    if ($c->req->param('fmt') eq 'json') {
+        $data = _make_class_testdata();
+        my $json = JSON::Syck::Dump($data);
+        $c->render('api/class/default.json', { json => $json });
+    }
+    else {
+        $c->render('api/class/no_items.tt');
+    }
 };
 
 any '/api/test' => sub {
@@ -157,15 +164,60 @@ sub _make_pc_testdata {
 
 sub _make_class_testdata {
     return [{
+        id => 1,
+        status => 1,
+        week => 'mon',
         period => 1,
         building => 63,
         room => 304,
         lan => OK,
+        outlet => OK,
+        wireless => OK,
+        room_size => 2,
     }, {
+        id => 3,
+        status => 1,
+        week => 'mon',
         period => 3,
         building => 54,
-        room => 102,
+        room => 203,
+        lan => OK,
+        outlet => OK,
+        wireless => NO,
+        room_size => 2,
+    }, {
+        id => 5,
+        status => 1,
+        week => 'mon',
+        period => 6,
+        building => 54,
+        room => 104,
+        lan => OK,
+        outlet => OK,
+        wireless => NO,
+        room_size => 2,
+    }, {
+        id => 6,
+        status => 1,
+        week => 'mon',
+        period => 6,
+        building => 53,
+        room => 303,
         lan => NO,
+        outlet => OK,
+        wireless => NO,
+        room_size => 1,
+    }, {
+        id => 10,
+        status => 1,
+        week => 'mon',
+        period => 7,
+        building => 52,
+        room => 104,
+        lan => OK,
+        outlet => OK,
+        wireless => OK,
+        room_size => 3,
     }];
 }
 
